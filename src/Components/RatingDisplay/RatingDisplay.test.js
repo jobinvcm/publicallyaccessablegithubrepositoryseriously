cc689e31d64028c45b564d1d41bb66a104497bfa
Jobin Mathew <jobinvcm@gmail.com>
2019-03-31 14:28:10 +1000

    Adding still more with coverage

--- src/Components/RatingDisplay/RatingDisplay.test.js
+++ src/Components/RatingDisplay/RatingDisplay.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount} from 'enzyme';
 import RatingDisplay from '.';
 
 describe('<RatingDisplay />', () => {
   it('renders RatingDisplay component', () => {
-    const wrapper = shallow(<RatingDisplay rating={3.5} />);
+    const wrapper = mount(<RatingDisplay rating={3.5} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Give return correct value', () => {
+    const wrapper = shallow(<RatingDisplay rating={3.5}/>);
+    expect(wrapper.text()).toEqual("35%");
   });
 });
